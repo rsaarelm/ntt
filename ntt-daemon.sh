da6626b4207b7e80ed55b1a639859a5ee2fde601
Risto Saarelma <risto.saarelma@iki.fi>
2024-03-13 00:46:51 +0200

    Daemon loop script

---
+++ ntt-daemon.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Exit script if ntt sleep is exited with Ctrl-C.
+set -e
+
+# Run a loop that notifies the user on every ping.
+# ntt must be compiled and in path before this is run.
+while [ true ]; do
+    ntt sleep
+    xrefresh -solid orange
+    notify-send ping
+    # Uncomment if you want to snapshot your desktop at ping time.
+    # scrot "/tmp/screenshot-$(date +'%Y-%m-%dT%H:%M:%S%z').png"
+done
